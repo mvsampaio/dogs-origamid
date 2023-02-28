2f5a3b8305d51f0b59abc18efe5713cbc594d55d
Marcus Sampaio <sampaiosoftware@gmail.com>
2023-02-28 10:00:52 -0300

    commit inicial da rede social dogs - estrutura já pronta para implementar a tela de login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login/*" element={<Login />}></Route>
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
