0817b9cf7f21509993bd0147a37062f239de88b3
Marcus Sampaio <sampaiosoftware@gmail.com>
2023-03-06 15:06:35 -0300

    css do footer

--- src/App.js
+++ src/App.js
@@ -14,25 +14,27 @@ import NotFound from './Components/NotFound';
 
 const App = () => {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="login/*" element={<Login />}></Route>
-            <Route
-              path="conta/*"
-              element={
-                <ProtectedRoute>
-                  <User />
-                </ProtectedRoute>
-              }
-            ></Route>
-            <Route path="foto/:id" element={<Photo />}></Route>
-            <Route path="perfil/:user" element={<UserProfile />}></Route>
-            <Route path="*" element={<NotFound />}></Route>
-          </Routes>
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="login/*" element={<Login />}></Route>
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route path="foto/:id" element={<Photo />}></Route>
+              <Route path="perfil/:user" element={<UserProfile />}></Route>
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
